3b36f0d0faf880182459c2beced249cffccc8ed0
ZmTrans-004 <zmtrans-004@192.168.1.30>
2026-06-22 11:18:34 +0000

    Ajout des fichiers du update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Local: keep http://127.0.0.1:5000 while developing.
 // Production: replace this with your Render backend URL, for example:
 // window.API_BASE_URL = "https://zmtrans-facturation-api.onrender.com";
-window.API_BASE_URL = "http://127.0.0.1:5000";
+window.API_BASE_URL = "https://facturationzmtrans.onrender.com";
